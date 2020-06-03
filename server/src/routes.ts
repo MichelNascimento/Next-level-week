43e2a2cf63d9cea61f1a648cea01ef7fe0c09548
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-06-03 19:37:24 -0300

    Criação da função de listagem de pontos

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import PointController from './controllers/PointsControllers';
-import ItemsController from './controllers/itemsControllers';
+import ItemsController from './controllers/ItemsControllers';
 
 const routes = express.Router();
 const pointsController = new PointController();
@@ -10,5 +10,7 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
