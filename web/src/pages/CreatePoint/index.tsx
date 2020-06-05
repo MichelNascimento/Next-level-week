6027ab221564fcd7bbefd9a82c650d2152fa54d0
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-06-04 21:13:07 -0300

    Pegando localização dinâmicamente no mapa

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
+import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 
 import './styles.css';
@@ -29,6 +30,9 @@ const CreatePoint = () => {
     const [ufs, setUfs] = useState<string[]>([]);
     const [selectedUf, setselectedUf] = useState('0');
     const [cities, setCities] = useState<string[]>([]);
+    const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [initialPosition, setInicialPosition] = useState<[number, number]>([0, 0]);
 
     // buscando os itens da api.
     useEffect(() => {
@@ -49,7 +53,7 @@ const CreatePoint = () => {
 
     // buscando as cidades da api do IBGE.
     useEffect(() => {
-        if(selectedUf === '0') {
+        if (selectedUf === '0') {
             return;
         }
 
@@ -61,10 +65,31 @@ const CreatePoint = () => {
             })
     }, [selectedUf])
 
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+
+            setInicialPosition([latitude, longitude]);
+        })
+    }, [])
+
     function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
         const uf = event.target.value;
 
         setselectedUf(uf);
+    }
+
+    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
+        const city = event.target.value;
+
+        setSelectedCity(city);
+    }
+
+    function handleMapClick(event: LeafletMouseEvent) {
+        setSelectedPosition([
+            event.latlng.lat,
+            event.latlng.lng
+        ])
     }
 
     return (
@@ -119,13 +144,13 @@ const CreatePoint = () => {
                     </legend>
 
                     {/* recebe primeiro latitude, depois longitude. */}
-                    <Map center={[-23.6384172, -46.757523]} zoom={15}>
+                    <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
 
-                        <Marker position={[-23.6384172, -46.757523]} />
+                        <Marker position={selectedPosition} />
                     </Map>
 
                     <div className="field-group">
@@ -146,7 +171,12 @@ const CreatePoint = () => {
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
+                            <select
+                                name="city"
+                                id="city"
+                                value={selectedCity}
+                                onChange={handleSelectCity}
+                            >
                                 <option value="0">Selecione uma cidade</option>
                                 {cities.map(city => (
                                     <option key={city} value={city}>{city}</option>
