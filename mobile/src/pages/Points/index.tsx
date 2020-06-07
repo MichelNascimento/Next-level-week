28e4022d18b78cd42126b13c6aa9c092201b59bf
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-06-07 14:09:15 -0300

    Configuração do upload de imagem

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image, Alert } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
@@ -18,8 +18,14 @@ interface Point {
     id: number,
     name: string,
     image: string,
+    image_url: string,
     latitude: number,
     longitude: number;
+}
+
+interface Params {
+    uf: string;
+    city: string;
 }
 
 const Points = () => {
@@ -29,6 +35,9 @@ const Points = () => {
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
     const navigation = useNavigation();
+    const route = useRoute();
+
+    const routeParams = route.params as Params;
 
     useEffect(() => {
         async function loadPosition() {
@@ -58,14 +67,14 @@ const Points = () => {
     useEffect(() => {
         api.get('poinst', {
             params: {
-                city: 'São Paulo',
-                uf: 'SP',
-                items: [1, 2]
+                city: routeParams.city,
+                uf: routeParams.uf,
+                items: selectedItems
             }
         }).then(response => {
             setPoints(response.data);
         })
-    }, [])
+    }, [selectedItems])
 
     function handleNavigateToBack() {
         navigation.goBack();
@@ -119,7 +128,7 @@ const Points = () => {
                                     }}
                                 >
                                     <View style={styles.mapMarkerContainer}>
-                                        <Image style={styles.mapMarkerImage} source={{ uri: point.image }} />
+                                        <Image style={styles.mapMarkerImage} source={{ uri: point.image_url }} />
                                         <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                                     </View>
                                 </Marker>
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,23 @@
 import express from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import PointController from './controllers/PointsControllers';
 import ItemsController from './controllers/ItemsControllers';
 
 const routes = express.Router();
+
+const upload = multer(multerConfig);
+
 const pointsController = new PointController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsController.create);
+// rota de cadastro de ponto, junto com o upload de imagem.
+routes.post('/points', upload.single('image'), pointsController.create);
+
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
